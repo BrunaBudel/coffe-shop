18f8e7881df10896c4d6eea82e676fa2a5dc5e3a
BrunaBudel <brunakbudel@gmail.com>
2023-08-28 09:23:53 -0300

    chore: added fonts

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/* eslint-disable camelcase */
+import { Text, View } from 'react-native'
+import {
+  useFonts,
+  Urbanist_700Bold,
+  Urbanist_400Regular,
+} from '@expo-google-fonts/dev'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Urbanist_700Bold, Urbanist_400Regular })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontsLoaded ? (
+        <Text>Open up App.tsx to start working on your app!</Text>
+      ) : (
+        <View />
+      )}
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
